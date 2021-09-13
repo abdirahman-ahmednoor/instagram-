d849a00ac85298c972da706acdf16cee84f8977e
BLADE <abdirahman.ahmed@student.moringaschool.com>
2021-09-13 08:56:42 +0300

    upadate urls.py file

--- clone/views.py
+++ clone/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from .models import *
 from django.contrib.auth.decorators import login_required
 from django.views.generic import CreateView

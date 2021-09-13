904fd8f81e2844a7c461375516746783fd7a9440
BLADE <abdirahman.ahmed@student.moringaschool.com>
2021-09-13 15:42:31 +0300

    updated tests.py file

--- clone/tests.py
+++ clone/tests.py
@@ -6,7 +6,7 @@ from django.contrib.auth import login, authenticate
 # Create your tests here.
 class TestProfile(TestCase):
     def setUp(self):
-        self.user = User(username='abdirahman', email = 'abdirahman.com', password = 'Password123')
+        self.user = User(username='abdirahman', email = 'abdirahman@gmail.com', password = 'Password123')
         self.user.save()
 
         self.profile_test = Profile(id=1, name='image', profile_picture='default.jpg', bio='this is a test profile',user=self.user)
